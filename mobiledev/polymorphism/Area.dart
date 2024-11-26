cf575a45307f43c7cadf18aaf13fc98043726ae6
iradukun <mustaphairadukunda@gmail.com>
2024-11-26 15:19:10 +0200

    [feat]: Recursion

---
+++ mobiledev/polymorphism/Area.dart
@@ -0,0 +1,24 @@
+import 'dart:math'; // Provides the 'pi' constant
+
+class Shape {
+  double area() {
+    print("Calculating area for a generic shape...");
+    return 0.0; 
+  }
+}
+
+class Circle extends Shape {
+  final double radius;
+
+  Circle(this.radius);
+
+  // @override
+  double area() {
+    return pi * radius * radius;
+  }
+}
+
+void main() {
+  Shape shape = Circle(7); 
+  print("Area of the Circle: ${shape.area()}");
+}
